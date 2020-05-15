1c56dda096bbc8f09e77b54fe44002965fa538fe
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2020-05-15 13:23:24 -0300

    chore(ci): make s3cmd accessible from runner

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/sh -e
 
-mkdir -p $GITHUB_WORKSPACE/bin
-ln -s $(which s3cmd) $GITHUB_WORKSPACE/bin/s3cmd
-
 # Make it accessible from docker containers
-echo "::add-path::$GITHUB_WORKSPACE/bin"
+echo "::add-path::$(dirname $(which s3cmd))"
 
 # Make it accessible from runner
 echo "::add-path::$RUNNER_WORKSPACE/$(basename $GITHUB_REPOSITORY)/bin"
