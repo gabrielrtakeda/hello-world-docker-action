d6359a6947774818fc6ad4fea00630cc5cdc79ec
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2020-05-15 12:49:18 -0300

    chore(ci): make it executable

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,7 @@
 
 mkdir -p $GITHUB_WORKSPACE/bin
 echo "echo Hello, world!" > $GITHUB_WORKSPACE/bin/s3cmd
+chmod +x $GITHUB_WORKSPACE/bin/s3cmd
+
 echo "::add-path::$GITHUB_WORKSPACE/bin" # Make it accessible from docker containers
 echo "::add-path::$RUNNER_WORKSPACE/$(basename $GITHUB_REPOSITORY)/bin" # Make it accessible from runner
