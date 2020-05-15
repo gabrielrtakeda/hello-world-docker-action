ebffb53eb66fb2b81173416403985c1d55c5fdaa
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2020-05-15 12:53:23 -0300

    chore(ci): enable s3cmd again

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -e
 
 mkdir -p $GITHUB_WORKSPACE/bin
-echo "echo Hello, world!" > $GITHUB_WORKSPACE/bin/s3cmd
-chmod +x $GITHUB_WORKSPACE/bin/s3cmd
+mv $(which s3cmd) $GITHUB_WORKSPACE/bin/
 
-echo "::add-path::$GITHUB_WORKSPACE/bin" # Make it accessible from docker containers
-echo "::add-path::$RUNNER_WORKSPACE/$(basename $GITHUB_REPOSITORY)/bin" # Make it accessible from runner
+# Make it accessible from docker containers
+echo "::add-path::$GITHUB_WORKSPACE/bin"
+
+# Make it accessible from runner
+echo "::add-path::$RUNNER_WORKSPACE/$(basename $GITHUB_REPOSITORY)/bin"
