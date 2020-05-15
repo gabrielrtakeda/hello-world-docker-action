c9d9ddf40cf83ec450bb9be54c5347ccb1eb33ab
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2020-05-15 11:28:44 -0300

    chore: convert input case from kebab to snake

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ printenv
 echo "Hello $1"
 echo "Goodbye $1"
 echo "::debug:: [debug] Hello $1"
-echo "::debug:: [debug-env] Hello $INPUT_WHOTOGREET"
+echo "::debug:: [debug-env] Hello $INPUT_WHO_TO_GREET"
 time=$(date)
 echo "::set-output name=time::$time"
