1c8983f2d14a33d6a601a5c6d9820884e49b2bc5
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2020-05-15 12:13:10 -0300

    chore(ci): use single cmd input

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,11 @@
 #!/bin/sh -e
 
-echo "::debug:: [access_key] $INPUT_ACCESS_KEY"
-echo "::debug:: [secret_key] $INPUT_SECRET_KEY"
 echo "::debug:: [cmd] $INPUT_CMD"
 
 s3cmd \
-  --no-check-certificate \
-  --access_key=$INPUT_ACCESS_KEY \
-  --secret_key=$INPUT_SECRET_KEY \
   --verbose \
-  --human-readable-sizes \
   --stop-on-error \
+  --no-check-certificate \
+  --human-readable-sizes \
   $INPUT_CMD
 
