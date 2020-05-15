0729a36e43e1dcdd13fc405612f8a2dfe89fcb58
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2020-05-15 11:20:20 -0300

    fix: remove underscore from env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "Hello $1"
 echo "Goodbye $1"
-echo "::debug:: [debug] Hello $1}"
-echo "::debug:: [debug-env] Hello $INPUT_WHO_TO_GREET}"
+echo "::debug:: [debug] Hello $1"
+echo "::debug:: [debug-env] Hello $INPUT_WHOTOGREET"
 time=$(date)
 echo "::set-output name=time::$time"
